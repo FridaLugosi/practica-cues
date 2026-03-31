65f76c82401f23a7fc6f09009d0b898a6bef48e4
Dongjun <ldongjun2005@gmail.com>
2026-03-31 17:32:43 +0200

    Add files via upload

---
+++ practica_cues.R
@@ -0,0 +1,39 @@
+# APARTAT 1
+ann <- read.table("A14.DAT", header = FALSE, dec = ",", fileEncoding = "UTF-16LE")$V1
+smm <- read.table("S12.DAT", header = FALSE, dec = ",", fileEncoding = "UTF-16LE")$V1
+
+(lambda.hat <- length(ann)/sum(ann))
+(mu.hat <- length(smm)/sum(smm))
+(rho.hat <- lambda.hat/mu.hat)
+
+summary(ann)
+sd(ann)
+(cv_ann <- sd(ann_vec) / mean(ann_vec))
+summary(smm)
+sd(smm)
+(cv_smm <- sd(smm_vec) / mean(smm_vec))
+
+par(mfrow=c(1,2)) 
+hist(ann, main="Temps entre arribades", col="lightblue")
+hist(smm, main="Temps de servei", col="lightgreen")
+
+# APARTAT 2
+n <- length(ann)
+
+x_menys <- qchisq(0.025,2*n)/(2*n)
+x_mes <- qchisq(1-0.025,2*n)/(2*n)
+c(lambda.hat/(2*n)*x_menys, lambda.hat/(2*n)*x_mes)
+
+f_menys <- qf(0.025,2*n,2*n)
+f_mes <- qf(1-0.025,2*n,2*n)
+c(rho.hat * f_menys, rho.hat * f_mes)
+
+
+(apartat2 <- data.frame( n = length(ann),
+                         f_menys = qf(0.025,2*n,2*n),
+                         f_mes = qf(1-0.025,2*n,2*n),
+                         ef = (qf(1-0.025,2*n,2*n)-qf(0.025,2*n,2*n))/mean(c(qf(1-0.025,2*n,2*n), qf(0.025,2*n,2*n)))*100,
+                         x_menys = qchisq(0.025,2*n)/(2*n),
+                         x_mes = qchisq(1-0.025,2*n)/(2*n),
+                         ex = (qchisq(1-0.025,2*n,2*n)-qchisq(0.025,2*n,2*n))/mean(c(qchisq(1-0.025,2*n,2*n), qchisq(0.025,2*n,2*n)))*100))
+
